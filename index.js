65eade76d2f6efd0b5f9f3e4291d4b3721eeb95a
Elena Lucci <elena.lucci@gmail.com>
2025-06-03 12:56:54 +0200

    Inserite card e prima stilizzazione

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const container = document.getElementById("container");
 async function fetchProducts() {
   try {
-    const response = await fetch("https://api.escuelajs.co/api/v1/products");
+    const response = await fetch("https://fakestoreapi.com/products");
     const result = await response.json();
     return result;
   } catch (error) {
@@ -12,9 +13,28 @@ async function renderProducts() {
   try {
     const products = await fetchProducts();
     products.forEach((x) => {
-      const h1 = document.createElement("h1");
-      h1.innerText = x.title;
-      document.body.appendChild(h1);
+      const card = document.createElement("div");
+      const title = document.createElement("h6");
+      const imageContainer = document.createElement("div");
+      const image = document.createElement("img");
+      const price = document.createElement("p");
+      const buy = document.createElement("button");
+      card.className = "card";
+      title.textContent = x.title;
+      image.alt = x.title;
+      image.src = x.image;
+      price.textContent = `${x.price} $`;
+      buy.textContent = "Buy";
+      buy.addEventListener("click", () => {
+        localStorage.setItem("chart", JSON.stringify(x))
+      });
+      imageContainer.appendChild(image);
+      card.appendChild(imageContainer);
+      card.appendChild(title);
+      card.appendChild(price);
+      card.appendChild(buy);
+      container.appendChild(card);
+
     });
   } catch (error) {
     console.error(error);
