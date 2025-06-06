02c70d65a9ce37346d5964b71abd36830d2a1412
francesca.castangia <castangiaf2@gmail.com>
2025-06-06 10:57:17 +0200

    aggiunti pulsanti + -

--- chart.js
+++ chart.js
@@ -1,13 +1,15 @@
 const prodottiChart = document.getElementById("prodotti-chart");
 const total = document.getElementById("total");
 let totaleCarrello = 0;
+const button = document.querySelector("#acquista");
 
 //Logica chart: siccome ciò che è salvato in localStorage è comune a tutte le pagine, non devo importare dei moduli in questa pagina, dalla index.js.
 
 const localChart = JSON.parse(localStorage.getItem("chart"));
 if (localChart.length > 0) {
+button.disabled = false;
+prodottiChart.innerHTML= " ";
   let arrayProductsQuantity = [];
-  console.log(localChart);
   localChart.forEach((x) => {
     const productsExist = arrayProductsQuantity.find((y) => x.id === y.id);
     if (!productsExist) {
@@ -25,21 +27,47 @@ if (localChart.length > 0) {
     const title = document.createElement("h6");
     const price = document.createElement("p");
     const quantity = document.createElement("p");
+    const addButton = document.createElement("button");
+    const removeButton = document.createElement("button");
 
+    addButton.innerText = "+";
+    removeButton.innerText = "-";
+    addButton.addEventListener("click", ()=> {
+      x.quantity += 1;
+      quantity.innerText = x.quantity;
+      totaleCarrello += x.price ;
+      total.innerText = totaleCarrello;
+    });
+    removeButton.addEventListener("click", ()=> {
+      x.quantity -= 1;
+      quantity.innerText = x.quantity;
+      if(x.quantity === 0){
+        prodottiChart.removeChild(divContainer);
+      }
+      totaleCarrello -= x.price ;
+      total.innerText = totaleCarrello;
+    });
+    
     img.src = x.image;
     img.alt = x.title;
     title.innerText = x.title;
     price.textContent = `${x.price} $`;
     quantity.innerText = x.quantity;
 
+    
+
     divContainer.appendChild(img);
     divContainer.appendChild(title);
     divContainer.appendChild(price);
+    divContainer.appendChild(addButton);
+    divContainer.appendChild(removeButton);
     divContainer.appendChild(quantity);
     prodottiChart.appendChild(divContainer);
+    
     totaleCarrello += x.price * x.quantity;
   });
   total.innerText = totaleCarrello;
+
 }
 
 // La card da inserire deve contenere: img, title, price e quantity.
